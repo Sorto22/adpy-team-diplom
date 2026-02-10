004e09cf11853c2f21e6ee1762e84e353f66202e
Oksana Polulyakh <oks.polulyakh@gmail.com>
2026-02-10 07:16:50 +0300

    Add vkapi.py: VKUser, VKSex, VkClient, tests: empty

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ import os
 load_dotenv()
 
 class Config:
+    VK_TOKEN = os.getenv("VK_TOKEN")  # Имя переменной, а не значение
     BOT_TOKEN = os.getenv("BOT_TOKEN")  # Имя переменной, а не значение
     POSTGRES_URI = os.getenv("POSTGRES_URI")
     POSTGRES_HOST = os.getenv("POSTGRES_HOST")
---
+++ vkapi.py
@@ -0,0 +1,50 @@
+from dataclasses import dataclass
+from typing import List, Optional
+from enum import Enum
+from config import Config
+import requests
+
+@dataclass
+class VKUser:
+    id: int
+    first_name: str
+    last_name: str
+    profile_url: str
+    # photos: List[str]
+
+class VKSex(Enum):
+    WOMEN = 1
+    MEN = 2
+    ALL = 0
+
+
+
+class VkClient:
+    def __init__(self, token: str):
+        self.token = token
+
+    def _get_comon_params(self):
+        return {
+            'access_token': self.token,
+            'v': '5.199'
+        }
+
+    def search_users(self, city_id: int, age_from: int, age_to: int, sex: enum(VKSex)) -> List[VKUser]:
+        url = 'https://api.vk.com/method/users.search'
+        params = {
+            city_id: city_id,
+            age_from: age_from,
+            age_to: age_to,
+            sex: sex
+        }
+        response = requests.get(url, params={**self._get_comon_params(), **params})
+        data = response.json()
+        users = []
+        for item in data['response']['items']:
+            user_id = item['id']
+            first_name = item['first_name']
+            last_name = item['last_name']
+            profile_url = f"https://vk.com/id{user_id}"
+            users.append(VKUser(id=user_id, first_name=first_name, last_name=last_name, profile_url=profile_url))
+        return users
+
